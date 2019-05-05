cdbb3419a594269a92b4ced7069a26aed8ffab80
Derek McCammond <dmm545@drexel.edu>
2019-05-05 03:56:36 -0400

    While I wait for more feedback regarding routing, I have hardcoded the about section into app.js so I can start filling it out. I have put a little boilerplate of the information that should be in there.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,10 @@ export default class About extends Component {
   render() {
     return (
       <div className="about">
-        { this.props.children }
+        <h1>About the project:</h1>
+        <p>This project was created for the 2019 Philadelphia Codefest.</p>
+        <p>Hackers: Alia Yeszhanova, <a href="www.github.com/vlek">Derek McCammond</a>, James Poindexter, and Steve Slatky</p>
+
       </div>
     )
   }
