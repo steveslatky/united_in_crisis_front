4623b5f28e773c5622d642d55462e76427a9746b
Derek McCammond <dmm545@drexel.edu>
2019-05-05 09:26:40 -0400

    Making some progress. The cards look nicer, but I can't get them to populate based on their current state. Still working on it, the error I am getting is saying that it cannot convert undefined to an object. I'm going to try pulling just the part I'm doing out and working on it in a browser to see where exactly the issues lies.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,27 +3,44 @@ import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/es/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
     card: {
         maxWidth: 200,
+        margin: 20,
+        padding: 20,
+        display: "inline-block"
+    },
+    chat: {
     }
 };
 
-
+const question_states = [
+    {0: "Repeat question", 1: "Next question"},
+    {0: "Start over", 1: "Repeat question"},
+];
 
 class Chat extends React.Component {
   render() {
       const { classes } = this.props;
+      let { question_state } = 0;
     return (
       <div className="chat">
-        <Card className={classes.card}>
-            <CardContent>
-                <Typography>
-                    Hello World!
-                </Typography>
-            </CardContent>
-        </Card>
+          <Grid container justify="center" alignItems="center">
+            {Object.keys(question_states[question_state])
+                .map((question, i) => (
+                    <Grid item>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography>
+                                    {i.toString()}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+          </Grid>
       </div>
     )
   }
