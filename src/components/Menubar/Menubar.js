42f5eaace04ea0fe07b445cd86269d0addd44201
Derek McCammond <dmm545@drexel.edu>
2019-05-05 06:49:16 -0400

    Added a router, having really touched it yet. I did however flesh out the menu a bit and got the events to work correctly after wrestling with them for a while.
    The next step is to get the routes to work with the MenuItem's. I think
    that I may be having difficulty because I'm not looking to place the
    navigation in the right place. Maybe I do not even need to use MenuItems
    and instead need Routes. I dunno, I'm tired.

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -5,10 +5,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from '@material-ui/icons/Menu';
+import { Router, Route, Switch } from "react-router";
 
 import About from "../About/About";
 import Home from "../Home/Home";
 import Chat from "../Chat/Chat";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Link from "@material-ui/core/Link";
 
 const styles = {
     root: {
@@ -19,16 +24,44 @@ const styles = {
     }
 };
 
+const state = {
+    anchorEl: null,
+};
+
 function Menubar(props) {
     const { classes } = props;
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
 
     return (
         <div className={classes.root}>
             <AppBar position="static" color="default">
                 <Toolbar>
-                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    <IconButton
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="Menu"
+                        onClick={handleClick}
+                    >
                         <MenuIcon />
                     </IconButton>
+                        <Menu id="simple-menu"
+                              anchorEl={anchorEl}
+                              open={Boolean(anchorEl)}>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuItem onClick={handleClose} >Home</MenuItem>
+                                <MenuItem onClick={handleClose} >Chat</MenuItem>
+                                <MenuItem onClick={handleClose}>About</MenuItem>
+                            </ClickAwayListener>
+                        </Menu>
                 </Toolbar>
             </AppBar>
         </div>
